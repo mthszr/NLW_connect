51665f4af34af178dd63b72350dc9b45d640efd1
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-18 19:56:05 -0300

    refactor home page; move subscription form to a separate component and update layout for improved organization and readability

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,7 @@
-import { Button } from '@/components/button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { Mail, Radio, User } from 'lucide-react'
+import { Radio } from 'lucide-react'
 import Image from 'next/image'
-import logo from '../assets/logo.svg'
+import logo from '../../assets/logo.svg'
+import { SubscriptionForm } from './subscription-form'
 
 export default function Home() {
   return (
@@ -35,29 +34,8 @@ export default function Home() {
             Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito{' '}
           </p>
         </div>
-        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
 
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome Completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <Mail />
-              </InputIcon>
-              <InputField type="email" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-
-          <Button type="submit">Confirmar</Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   )
