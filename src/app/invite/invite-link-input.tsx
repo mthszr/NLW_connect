41bbb3a6f6ea8e468dc3d069552fcc969cb9c0f3
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-18 19:47:22 -0300

    refactor invite page; extract InviteLinkInput, Ranking, and Stats components for improved structure and readability

---
+++ src/app/invite/invite-link-input.tsx
@@ -0,0 +1,21 @@
+import { IconButton } from "@/components/icon-button";
+import { InputRoot, InputIcon, InputField } from "@/components/input";
+import { Link, Copy } from "lucide-react";
+
+export function InviteLinkInput() {
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link className="size-5" />
+      </InputIcon>
+      <InputField
+        readOnly
+        defaultValue="http://localhost:3000/invite/8239232810"
+      />
+
+      <IconButton className="-mr-2">
+        <Copy className="size-5" />
+      </IconButton>
+    </InputRoot>
+  )
+}
