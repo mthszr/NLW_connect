b0d3ada389391d42903ef0f9aa294efc88d222ba
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-18 14:58:06 -0300

    correct className to use flex layout for better alignment

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -7,7 +7,7 @@ export function InputRoot({ error = false, ...props }: InputRootProps) {
   return (
     <div
       data-error={error}
-      className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 items-center gap-2 focus-withing:border-gray-100 data-[error=true]:border-danger"
+      className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-withing:border-gray-100 data-[error=true]:border-danger"
       {...props}
     />
   )
