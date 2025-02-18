658134d52a28aec8fa8cc174fadf804a3b9b6b95
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-18 13:07:38 -0300

    Refactors input component structure
    Updates the input component structure to improve code organization and reusability. Replaces the previous implementation with separate components for input root, input icon, and input field. This change allows for better styling and flexibility in handling input errors.

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,26 +1,36 @@
-import { Mail } from 'lucide-react'
-
 import { ComponentProps } from "react";
-
-interface InputProps extends ComponentProps<'input'> { 
+interface InputRootProps extends ComponentProps<'div'> {
     error?: boolean
 }
 
-export function Input({ error = false, ...props}: InputProps) {
+export function InputRoot({ error = false, ...props }: InputRootProps) {
     return (
-        <div 
+        <div
             data-error={error}
             className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 items-center gap-2 focus-withing:border-gray-100 data-[error=true]:border-danger"
-        >
-            <span
-                className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
-            >
-                <Mail />
-            </span>
-            <input
-                className="flex-1 outline-0 placeholder-gray-400"
-                {...props}
-            />
-        </div>
+            {...props}
+        />
+    )
+}
+
+interface InputIconProps extends ComponentProps<'span'> {}
+
+export function InputIcon(props: InputIconProps) {
+    return (
+        <span
+            className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
+            {...props}
+        />
+    )
+}
+
+interface InputFieldProps extends ComponentProps<'input'> {}
+
+export function InputField(props: InputFieldProps) {
+    return (
+        <input
+            className="flex-1 outline-0 placeholder-gray-400"
+            {...props}
+        />
     )
 }
