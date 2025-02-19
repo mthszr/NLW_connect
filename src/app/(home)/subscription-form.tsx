9fa25cf223569a0ecf9d893a4cf558291693e582
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-19 11:34:40 -0300

    add orval configuration and API integration; implement subscription form with validation and API calls

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -1,8 +1,33 @@
+'use client'
+
+import { subscribeToEvent } from '@/http/api'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight, Mail, User } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Button } from '@/components/button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { Mail, User } from 'lucide-react'
+
+const subscriptionSchema = z.object({
+  name: z.string().min(2, 'Digite seu nome completo'),
+  email: z.string().email('Digite um e-mail válido'),
+})
+
+type SubscriptionSchema = z.infer<typeof subscriptionSchema>
 
 export function SubscriptionForm() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubscriptionSchema>({
+    resolver: zodResolver(subscriptionSchema),
+  })
+
   return (
     <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
       <h2 className="font-heading font-semibold text-gray-200 text-xl">
